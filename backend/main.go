b6c3fd7096cf737c91b2c14a0d2da3215d31a8c0
Hamid Tabrizi <tabrizihamid6367@gmail.com>
2025-08-06 13:44:39 +0330

    🚑 Add HealthCheck and setup the main.go endpoint to verify server status

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,43 @@
 package main
 
-func main(){
-	println("It is running")
+import (
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/tabrizihamid84/library-application/api/route"
+	"github.com/tabrizihamid84/library-application/bootstrap"
+)
+
+func main() {
+	app := bootstrap.App()
+
+	env := app.Env
+
+	db := app.Mongo.Database(env.DBName)
+	defer app.CloseDB()
+
+	timeout := time.Duration(env.ContextTimeout) * time.Second
+
+	echo := echo.New()
+
+	echo.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://labstack.com", "http://localhost:3000"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	route.Setup(env, timeout, db, echo)
+
+	echo.GET("/", HealthCheck)
+	// echo.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	echo.Logger.Fatal(echo.Start(env.ServerAddress))
 }
 
+func HealthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"data": "Server is up and running",
+	})
+
+}
