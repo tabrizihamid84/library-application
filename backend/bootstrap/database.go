ef27edc1ab30cb99f3b36150a010b4e5d5d3b065
Hamid Tabrizi <tabrizihamid6367@gmail.com>
2025-08-06 13:40:08 +0330

    🔌 Gracefully disconnect MongoDB client with timeout and logging

---
+++ backend/bootstrap/database.go
@@ -0,0 +1,59 @@
+package bootstrap
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/tabrizihamid84/library-application/mongo"
+)
+
+func ConnectDB(env *Env) mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	dbHost := env.DBHost
+	dbPort := env.DBPort
+	dbUser := env.DBUser
+	dbPass := env.DBPass
+
+	mongodbURI := fmt.Sprintf("mongodb://%s:%s@%s:%s/?authSource=admin&readPreference=primary&ssl=false&directConnection=true", dbUser, dbPass, dbHost, dbPort)
+
+	if dbUser == "" || dbPass == "" {
+		mongodbURI = fmt.Sprintf("mongodb://%s:%s", dbHost, dbPort)
+	}
+
+	client, err := mongo.NewClient(mongodbURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func CloseDB(client mongo.Client) {
+	if client == nil {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err := client.Disconnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connection to MongoDB closed.")
+}
