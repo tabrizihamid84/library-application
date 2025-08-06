d08eedfec279b1c9fec87fb42919b1d32fdf1859
Hamid Tabrizi <tabrizihamid6367@gmail.com>
2025-08-06 14:06:16 +0330

    ✅ Implement GetById and Update and Delete methods in bookUsecase

--- backend/usecase/book_usecase.go
+++ backend/usecase/book_usecase.go
@@ -20,26 +20,26 @@ func NewBookUsecase(bookRepository domain.BookRepository, timeout time.Duration)
 }
 
 // Create implements domain.BookUsecase.
-func (u *bookUsecase) Create(c context.Context, book *domain.Book) error {
-	return u.bookRepository.Create(c, book)
+func (u *bookUsecase) Create(ctx context.Context, book *domain.Book) error {
+	return u.bookRepository.Create(ctx, book)
 }
 
 // Delete implements domain.BookUsecase.
-func (*bookUsecase) Delete(ctx context.Context, id int) error {
-	panic("unimplemented")
+func (u *bookUsecase) Delete(ctx context.Context, id int) error {
+	return u.bookRepository.Delete(ctx, id)
 }
 
 // GetAll implements domain.BookUsecase.
-func (u *bookUsecase) GetAll(c context.Context) ([]domain.Book, error) {
-	return u.bookRepository.GetAll(c)
+func (u *bookUsecase) GetAll(ctx context.Context) ([]domain.Book, error) {
+	return u.bookRepository.GetAll(ctx)
 }
 
 // GetById implements domain.BookUsecase.
-func (*bookUsecase) GetById(ctx context.Context, id int) (domain.Book, error) {
-	panic("unimplemented")
+func (u *bookUsecase) GetById(ctx context.Context, id int) (domain.Book, error) {
+	return u.bookRepository.GetById(ctx, id)
 }
 
 // Update implements domain.BookUsecase.
-func (*bookUsecase) Update(ctx context.Context, id int, book *domain.Book) error {
-	panic("unimplemented")
+func (u *bookUsecase) Update(ctx context.Context, id int, book *domain.Book) error {
+	return u.bookRepository.Update(ctx, id, book)
 }
