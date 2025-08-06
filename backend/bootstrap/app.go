f052ee55d049e82a3387fcc5c20b0d07f2a7c305
Hamid Tabrizi <tabrizihamid6367@gmail.com>
2025-08-06 13:43:41 +0330

    🚀 Initialize Application struct with Env and MongoDB client setup

---
+++ backend/bootstrap/app.go
@@ -0,0 +1,21 @@
+package bootstrap
+
+import (
+	"github.com/tabrizihamid84/library-application/mongo"
+)
+
+type Application struct {
+	Env   *Env
+	Mongo mongo.Client
+}
+
+func App() Application {
+	app := &Application{}
+	app.Env = LoadEnv()
+	app.Mongo = ConnectDB(app.Env)
+	return *app
+}
+
+func (app *Application) CloseDB() {
+	CloseDB(app.Mongo)
+}
